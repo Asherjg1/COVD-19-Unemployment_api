0236a23a1552ff582f1ae2e174421bc505723f08
asherjg1 <asherjg1@gmail.com>
2020-05-31 15:25:27 -0700

    q

--- app/jobs/hello_world.rb
+++ app/jobs/hello_world.rb
@@ -1,12 +1,14 @@
 class HelloWorld < ApplicationJob
   require 'selenium-WebDriver'
-  
+
   queue_as :low_priority
   def perform()
     require 'selenium-WebDriver'
   @browser=Selenium::WebDriver.for :chrome
-  @browser.navigate.to "https://www.supremenewyork.com/"
+  @browser.navigate.to "https://airtable.com/embed/shrqYt5kSqMzHV9R5?backgroundColor=green&viewControls=on"
   sleep 2
-  @browser.find_element(class: 'shop_link').click
+  @browser.find_element(class: 'viewMenuPopover').click
+  sleep 1
+  @browser.find_element(class: 'viewMenuButton.pointer')
   end
 end
